9db09812670d513d34b62f6a72e45b9410f7b28f
김영후 <eagle7449@naver.com>
2023-07-22 22:49:00 +0900

    모듈 분리

---
+++ manager/src/main/java/com/rest/api/ManagerApplication.java
@@ -0,0 +1,10 @@
+package com.rest.api;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManagerApplication {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
