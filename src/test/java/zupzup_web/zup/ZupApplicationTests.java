2c665fb781b43f3dfabd61f5beec6621409de313
seasameoil <whdwlgus12@gmail.com>
2023-07-22 15:55:47 +0900

    first commit

---
+++ src/test/java/zupzup_web/zup/ZupApplicationTests.java
@@ -0,0 +1,13 @@
+package zupzup_web.zup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZupApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
