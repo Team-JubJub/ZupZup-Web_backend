04f922ce16f09f4565ee12dbf789a291b08d01da
김영후 <eagle7449@naver.com>
2023-07-27 22:51:30 +0900

    입점신청 도메인 생성 완료

--- seller/src/main/java/com/rest/api/domain/EnterReqs.java
+++ seller/src/main/java/com/rest/api/domain/EnterReqs.java
@@ -2,6 +2,7 @@ package com.rest.api.domain;
 
 import jakarta.persistence.*;
 import lombok.*;
+import org.hibernate.annotations.ColumnDefault;
 
 @Entity
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
@@ -10,5 +11,24 @@ import lombok.*;
 @Getter
 @Table(name = "enter_reqs")
 public class EnterReqs {
+    @Id
+    @Column(name = "enterReqId")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long enterReqId;
+
+    @JoinColumn(name = "seller")
+    private Long sellerId;  // seller와 매핑
+
+    @Column(nullable = false) private String storeName;
+    @Column(nullable = false) private String storeAddress;
+    @Column(nullable = false) private String crNumber;
+    @Column(nullable = false, columnDefinition = "boolean default false") private Boolean isAccepted;
+
+    public EnterReqsBuilder builder(Long enterReqId) {
+        if(enterReqId == null) {
+            throw new IllegalArgumentException("필수 파라미터(입점신청 ID) 누락");
+        }
+        return EnterReqsBuilder().enterReqId(enterReqId);
+    }
 
 }
