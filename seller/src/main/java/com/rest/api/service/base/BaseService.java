de43e984945e8c4efa39f953ec67505d2e7ce277
김영후 <eagle7449@naver.com>
2023-08-19 17:55:16 +0900

    BaseServiceImpl 생성 및 ModelMapperConfig 정의, 의존성 등록

--- seller/src/main/java/com/rest/api/service/base/BaseService.java
+++ seller/src/main/java/com/rest/api/service/base/BaseService.java
@@ -3,7 +3,13 @@ package com.rest.api.service.base;
 import com.rest.api.model.BaseEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface BaseService<E extends BaseEntity, Rq, Rs, R extends JpaRepository<E, Long>> {
 
+    List<Rs> findAllManger() throws Exception; // 사장님 전체 조회
+    Rs save(Rq rq) throws Exception; // 사장님 저장
+    Rs update(Rq rq) throws Exception; // 사장님 업데이트
+    Long delete(Long id) throws Exception; // 사장님 삭제
 
 }
