8a65543c7e5b53d320101ff89526283e5bb6afcf
seasameoil <whdwlgus12@gmail.com>
2023-09-25 01:17:37 +0900

    manager/result test 기본 파일 생성

---
+++ manager/src/test/java/com/zupzup/untact/api/ResultApiTest.java
@@ -0,0 +1,51 @@
+package com.zupzup.untact.api;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.zupzup.untact.documents.RestDocsConfig;
+import com.zupzup.untact.service.impl.EnterServiceImpl;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Import;
+import org.springframework.restdocs.RestDocumentationContextProvider;
+import org.springframework.restdocs.RestDocumentationExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.filter.CharacterEncodingFilter;
+
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.documentationConfiguration;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+
+@SpringBootTest
+@Import({RestDocsConfig.class})
+@ExtendWith({RestDocumentationExtension.class})
+@AutoConfigureMockMvc
+public class ResultApiTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @BeforeEach
+    public void before(WebApplicationContext ctx, RestDocumentationContextProvider restDocumentationContextProvider) {
+        this.mockMvc = MockMvcBuilders.webAppContextSetup(ctx)
+                .apply(documentationConfiguration(restDocumentationContextProvider))
+                .addFilters(new CharacterEncodingFilter("UTF-8", true))
+                .alwaysDo(print())
+                .build();
+    }
+
+    @Test
+    @DisplayName("신규 신청 매장 상세 - 성공")
+    public void success_new_detail() throws Exception {
+
+    }
+}
