880ce0fb4ad736a76dbc04d230a5cdfa125b1912
seasameoil <whdwlgus12@gmail.com>
2024-01-09 21:28:56 -0500

    fix : 에러 메세지 수정 및 status code 수정

--- member/src/main/java/com/zupzup/untact/exception/member/MemberExceptionType.java
+++ member/src/main/java/com/zupzup/untact/exception/member/MemberExceptionType.java
@@ -5,28 +5,27 @@ import org.springframework.http.HttpStatus;
 
 public enum MemberExceptionType implements BaseExceptionType {
     // 회원가입, 로그인 시
-    ALREADY_EXIST_USERNAME(600, HttpStatus.OK, "이미 존재하는 아이디입니다."),
-    WRONG_PASSWORD(601,HttpStatus.OK, "비밀번호가 잘못되었습니다."),
-    NOT_FOUND_MEMBER(602, HttpStatus.OK, "회원 정보가 없습니다."),
-    NOT_SAME_PASSWORD(603, HttpStatus.OK, "비밀번호가 같지 않습니다."),
-    CANNOT_USE_SAME_PASSWORD(604, HttpStatus.OK, "이전 비밀번호와 같은 비밀번호를 사용할 수 없습니다."),
-    APPLE_CANNOT_FIND_MEMBER(605, HttpStatus.BAD_REQUEST, "회원 정보를 찾을 수 없습니다."),
-    SERVER_ERR(666, HttpStatus.EXPECTATION_FAILED, "저장에 실패하였습니다. 다시 시도해주세요.");
+    ALREADY_EXIST_USERNAME(HttpStatus.BAD_REQUEST, "이미 존재하는 아이디입니다."),
+    WRONG_PASSWORD(HttpStatus.BAD_REQUEST, "비밀번호가 잘못되었습니다."),
+    NOT_FOUND_MEMBER(HttpStatus.BAD_REQUEST, "회원 정보가 없습니다."),
+    NOT_SAME_PASSWORD(HttpStatus.BAD_REQUEST, "비밀번호가 같지 않습니다."),
+    CANNOT_USE_SAME_PASSWORD(HttpStatus.BAD_REQUEST, "이전 비밀번호와 같은 비밀번호를 사용할 수 없습니다."),
+    SERVER_ERR(HttpStatus.EXPECTATION_FAILED, "저장에 실패하였습니다. 다시 시도해주세요.");
 
     private int errorCode;
     private HttpStatus httpStatus;
     private String errorMessage;
 
-    MemberExceptionType(int errorCode, HttpStatus httpStatus, String errorMessage) {
-        this.errorCode = errorCode;
+    MemberExceptionType(HttpStatus httpStatus, String errorMessage) {
+//        this.errorCode = errorCode;
         this.httpStatus = httpStatus;
         this.errorMessage = errorMessage;
     }
 
-    @Override
-    public int getErrCode() {
-        return this.errorCode;
-    }
+//    @Override
+//    public int getErrCode() {
+//        return this.errorCode;
+//    }
 
     @Override
     public HttpStatus getHttpStatus() {
