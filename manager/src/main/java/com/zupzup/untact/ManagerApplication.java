ebdb8d0555b2a2e840fc632a8ec7f847e9827af4
seasameoil <whdwlgus12@gmail.com>
2024-02-09 00:22:09 -0500

    doc: manager api 탈퇴 전까지 문서화

--- manager/src/main/java/com/zupzup/untact/ManagerApplication.java
+++ manager/src/main/java/com/zupzup/untact/ManagerApplication.java
@@ -1,26 +1,30 @@
-//package com.zupzup.untact;
-//
-//import org.springframework.boot.SpringApplication;
-//import org.springframework.boot.autoconfigure.SpringBootApplication;
-//import org.springframework.boot.autoconfigure.domain.EntityScan;
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-//import org.springframework.scheduling.annotation.EnableAsync;
-//import org.springframework.scheduling.annotation.EnableScheduling;
-//import org.springframework.web.filter.HiddenHttpMethodFilter;
-//
-//@SpringBootApplication
+package com.zupzup.untact;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.web.filter.HiddenHttpMethodFilter;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 //@EntityScan(basePackages = {"com.zupzup"})
 //@EnableJpaRepositories(basePackages = {"com.zupzup.untact.repository"})
 //@EnableAsync
 //@EnableScheduling
-//public class ManagerApplication {
-//    public static void main(String[] args) {
-//        SpringApplication.run(ManagerApplication.class, args);
-//    }
+public class ManagerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ManagerApplication.class, args);
+    }
 //
 //    @Bean
 //    public HiddenHttpMethodFilter hiddenHttpMethodFilter() {
 //        return new HiddenHttpMethodFilter();
 //    }
-//}
+}
